f3e26a7b3f677cc4bf5b62ff08ecad9cc345bc85
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-01 15:06:39 -0500

    p55: work around "this" scope with bind

--- weather/components/SearchInput.tsx
+++ weather/components/SearchInput.tsx
@@ -16,7 +16,7 @@ export default class SearchInput extends React.Component<{ placeholder: string }
                     underlineColorAndroid="transparent"
                     style={styles.textInput}
                     clearButtonMode="always"
-                    onChangeText={this.handleChangeText}
+                    onChangeText={this.handleChangeText.bind(this)}
                 />
             </View>
         );
