2cb7b1774a00b11cebf49da647d3988f15a2b607
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-01 01:07:00 -0500

    page 52

--- weather/App.tsx
+++ weather/App.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import {KeyboardAvoidingView, Platform, StyleSheet, Text} from 'react-native';
+import {ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text, View} from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
     render() {
         return (
             <KeyboardAvoidingView style={styles.container} behavior="height">
-                <Text style={[styles.largeText, styles.textStyle]}>
-                    San Francisco
-                </Text>
-                <Text style={[styles.smallText, styles.textStyle]}>
-                    Light Cloud
-                </Text>
-                <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+                <ImageBackground
+                    source={getImageForWeather('Clear')}
+                    style={styles.imageContainer}
+                    imageStyle={styles.image}
+                >
+                    <View style={styles.detailsContainer}>
+                        <Text style={[styles.largeText, styles.textStyle]}>
+                            San Francisco
+                        </Text>
+                        <Text style={[styles.smallText, styles.textStyle]}>
+                            Light Cloud
+                        </Text>
+                        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-                <SearchInput placeholder="Search any city" />
+                        <SearchInput placeholder="Search any city"/>
+                    </View>
+                </ImageBackground>
             </KeyboardAvoidingView>
         );
     }
@@ -27,13 +37,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    imageContainer: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover',
+    },
+    detailsContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.2)',
+        paddingHorizontal: 20,
+    },
     textStyle: {
         textAlign: 'center',
         // fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
         ...Platform.select({
             ios: {fontFamily: 'AvenirNext-Regular'},
             android: {fontFamily: 'Roboto'}
-        })
+        }),
+        color: 'white',
     },
     largeText: {
         fontSize: 44,
