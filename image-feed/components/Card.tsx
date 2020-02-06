24f2c837d30b954d76472a8b617a20f024eac3f2
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-05 20:25:31 -0500

    p215: add onLoad handler to Card

--- image-feed/components/Card.tsx
+++ image-feed/components/Card.tsx
@@ -10,20 +10,33 @@ interface P {
     onPressLinkText?: (event: GestureResponderEvent) => void
 }
 
-export default class Card extends React.Component<P> {
+interface S {
+    loading: boolean
+}
+
+export default class Card extends React.Component<P, S> {
     static defaultProps = {
         linkText: '',
         onPressLinkText: () => {
         },
     };
 
+    state = {
+        loading: true
+    };
+
+    handleLoad = () => {
+        this.setState({loading: false});
+    };
+
     render() {
         const {fullname, image, linkText, onPressLinkText} = this.props;
+        const {loading} = this.state;
 
         return (
             <View>
                 <AuthorRow fullname={fullname} linkText={linkText} onPressLinkText={onPressLinkText} />
-                <Image style={styles.image} source={image} />
+                <Image style={styles.image} source={image} onLoad={this.handleLoad} />
             </View>
         );
     }
