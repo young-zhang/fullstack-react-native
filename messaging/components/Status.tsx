9cc9385649e08efabe079d1e5f8d92fa1c55210c
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-08 22:09:01 -0500

    p289: added messageContainer to Status.tsx

--- messaging/components/Status.tsx
+++ messaging/components/Status.tsx
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 import NetInfo from '@react-native-community/netinfo';
-import {Platform, StatusBar, StyleSheet, View} from 'react-native';
+import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 interface S {
@@ -20,10 +20,21 @@ export default class Status extends React.Component<{}, S> {
                                       barStyle={isConnected ? 'dark-content' : 'light-content'}
                                       animated={false} />);
 
+        const messageContainer = (
+            <View style={styles.messageContainer} pointerEvents={'none'}>
+                {statusBar}
+                {!isConnected && (
+                    <View style={styles.bubble}>
+                        <Text style={styles.text}>No network connection</Text>
+                    </View>
+                )}
+            </View>
+        );
+
         if (Platform.OS === 'ios') {
             return (<View style={[styles.status, {backgroundColor}]}>{messageContainer}</View>);
         }
-        return null; // Temporary!
+        return messageContainer;
     }
 }
 
@@ -32,5 +43,23 @@ const styles = StyleSheet.create({
     status: {
         zIndex: 1,
         height: statusHeight,
+    },
+    messageContainer: {
+        zIndex: 1,
+        position: 'absolute',
+        top: statusHeight + 20,
+        right: 0,
+        left: 0,
+        height: 80,
+        alignItems: 'center',
+    },
+    bubble: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        backgroundColor: 'red',
+    },
+    text: {
+        color: 'white',
     },
 });
