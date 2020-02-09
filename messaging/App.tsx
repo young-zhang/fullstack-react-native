203b7e1099cf26ac9bbf416f0bcda27e42567ca4
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-08 20:53:46 -0500

    p277: add messaging/App.tsx

--- messaging/App.tsx
+++ messaging/App.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import React, {ReactNode} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    renderMessageList(): ReactNode {
+        return (
+            <View style={styles.content} />
+        );
+    }
+
+    renderInputMethodEditor(): ReactNode {
+        return (
+            <View style={styles.inputMethodEditor} />
+        );
+    }
+
+    renderToolbar(): ReactNode {
+        return (
+            <View style={styles.toolbar} />
+        );
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.renderMessageList()}
+                {this.renderToolbar()}
+                {this.renderInputMethodEditor()}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    content: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    inputMethodEditor: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    toolbar: {
+        borderTopWidth: 1,
+        borderTopColor: 'rgba(0,0,0,0.04)',
+        backgroundColor: 'white',
+    },
 });
