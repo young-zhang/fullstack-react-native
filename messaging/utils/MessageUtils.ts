92e523994a0503ccf53ab91eadb36ce48222f993
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-09 20:32:43 -0500

    p296: add message creation functions

--- messaging/utils/MessageUtils.ts
+++ messaging/utils/MessageUtils.ts
@@ -1,10 +1,43 @@
+export interface Coordinate {
+    latitude: number
+    longitude: number
+}
+
 export interface MessageShape {
     id: number
     type: 'text' | 'image' | 'location',
     text?: string
     uri?: string
-    coordinate?: {
-        latitude: number
-        longitude: number
-    }
+    coordinate?: Coordinate
+}
+
+let messageId: number = 0;
+
+function getNextId() {
+    messageId += 1;
+    return messageId;
+}
+
+export function createTextMessage(text: string): MessageShape {
+    return {
+        type: 'text',
+        id: getNextId(),
+        text
+    };
+}
+
+export function createImageMessage(uri: string): MessageShape {
+    return {
+        type: 'image',
+        id: getNextId(),
+        uri
+    };
+}
+
+export function createLocationMessage(coordinate: Coordinate): MessageShape {
+    return {
+        type: 'location',
+        id: getNextId(),
+        coordinate
+    };
 }
