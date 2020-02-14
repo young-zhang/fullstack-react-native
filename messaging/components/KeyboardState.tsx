972792ae40766a830b7414a2ae72deb52ffc420c
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-14 13:42:33 -0500

    p369: added KeyboardState.tsx

--- messaging/components/KeyboardState.tsx
+++ messaging/components/KeyboardState.tsx
@@ -1,4 +1,4 @@
-import {Keyboard, Platform} from "react-native";
+import {EmitterSubscription, Keyboard, KeyboardEvent, Platform} from "react-native";
 import React from 'react';
 
 interface Layout {
@@ -36,5 +36,90 @@ export default class KeyboardState extends React.Component<P, S> {
             keyboardWillHide: false,
             keyboardAnimationDuration: INITIAL_ANIMATION_DURATION,
         };
+    }
+
+    private subscriptions: EmitterSubscription[];
+
+    componentDidMount() {
+        if (Platform.OS === 'ios') {
+            this.subscriptions = [
+                Keyboard.addListener('keyboardWillShow', this.keyboardWillShow),
+                Keyboard.addListener('keyboardWillHide', this.keyboardWillHide),
+                Keyboard.addListener('keyboardDidShow', this.keyboardDidShow),
+                Keyboard.addListener('keyboardDidHide', this.keyboardDidHide),
+            ];
+        }
+        else {
+            this.subscriptions = [
+                Keyboard.addListener('keyboardDidHide', this.keyboardDidHide),
+                Keyboard.addListener('keyboardDidShow', this.keyboardDidShow),
+            ];
+        }
+    }
+
+    componentWillUnmount() {
+        this.subscriptions.forEach(subscription => subscription.remove());
+    }
+
+    keyboardWillShow = (event: KeyboardEvent) => {
+        this.setState({keyboardWillShow: true});
+        this.measure(event);
+    };
+
+    keyboardDidShow = (event: KeyboardEvent) => {
+        this.setState({
+            keyboardWillShow: false,
+            keyboardVisible: true,
+        });
+        this.measure(event);
+    };
+
+    keyboardWillHide = (event: KeyboardEvent) => {
+        this.setState({keyboardWillHide: true});
+        this.measure(event);
+    };
+
+    keyboardDidHide = () => {
+        this.setState({
+            keyboardWillHide: false,
+            keyboardVisible: false
+        });
+    };
+
+    measure = (event: KeyboardEvent) => {
+        const {layout} = this.props;
+
+        const {
+            endCoordinates: {height, screenY},
+            duration,
+        } = event;
+
+        this.setState({
+            contentHeight: screenY - layout.y,
+            keyboardHeight: height,
+            keyboardAnimationDuration: duration || INITIAL_ANIMATION_DURATION,
+        });
+    };
+
+    render() {
+        const {children, layout} = this.props;
+        const {
+            contentHeight,
+            keyboardHeight,
+            keyboardVisible,
+            keyboardWillShow,
+            keyboardWillHide,
+            keyboardAnimationDuration,
+        } = this.state;
+
+        return children({
+            containerHeight: layout.height,
+            contentHeight,
+            keyboardHeight,
+            keyboardVisible,
+            keyboardWillShow,
+            keyboardWillHide,
+            keyboardAnimationDuration,
+        });
     }
 }
