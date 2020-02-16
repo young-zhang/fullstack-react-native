fc0e9dc3f8f89ea0ad950149421cfba351613965
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-15 20:24:37 -0500

    p414: create routes.ts

---
+++ contact-list/routes.ts
@@ -0,0 +1,12 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
+
+const StackNavigator = createStackNavigator({
+    Contacts: {screen: Contacts},
+    Profile: {screen: Profile},
+});
+
+export default createAppContainer(StackNavigator);
