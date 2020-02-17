c2c5f32582c7d7c94b3cd473801714116716a7c5
Young Zhang <young.quinn.zhang@gmail.com>
2020-02-17 13:16:43 -0500

    p434: added Favorites.tsx

--- contact-list/routes.js
+++ contact-list/routes.js
@@ -3,14 +3,16 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import Favorites from './screens/Favorites';
 
 const StackNavigator = createStackNavigator(
     {
         Contacts,
         Profile,
+        Favorites,
     },
     {
-        initialRouteName: 'Contacts',
+        initialRouteName: 'Favorites',
     }
 );
 
